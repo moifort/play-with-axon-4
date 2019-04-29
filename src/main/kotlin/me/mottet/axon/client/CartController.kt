a3bb36bf8b240ef4c5f08f465d4e3ac01f8e9124
Thibaut Mottet <thibaut.mottet@gmai.com>
2019-04-29 18:15:40 +0200

    Firs init

--- src/main/kotlin/me/mottet/axon/client/CartController.kt
+++ src/main/kotlin/me/mottet/axon/client/CartController.kt
@@ -7,21 +7,20 @@ import me.mottet.axon.query.cart.CartDetailQuery
 import org.axonframework.commandhandling.gateway.CommandGateway
 import org.axonframework.messaging.responsetypes.ResponseTypes
 import org.axonframework.queryhandling.QueryGateway
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.*
 
 
 @RestController
+@RequestMapping("cart")
 class CartController(val commandGateway: CommandGateway, val queryGateway: QueryGateway) {
 
     @GetMapping("/create")
     fun createCart() = commandGateway.sendAndWait<UUID>(CreateCart(UUID.randomUUID(), UUID.randomUUID()))
 
     @GetMapping("/add")
-    fun createCart(@RequestParam cartId: String) = commandGateway.sendAndWait<UUID>(AddProduct(UUID.fromString(cartId), "Pantoufle", 3))
+    fun addProduct(@RequestParam cartId: String) = commandGateway.sendAndWait<UUID>(AddProduct(UUID.fromString(cartId), "Pantoufle", 3))
 
-    @GetMapping("/detail")
-    fun detail() = queryGateway.query(CartDetailQuery(UUID.fromString("8eb73ae1-5f26-4aab-81ca-31e51b2727b4")), ResponseTypes.instanceOf(CartDetail::class.java)).join()
+    @GetMapping("/{id}/detail")
+    fun detail(@PathVariable("id") cartId: String) = queryGateway.query(CartDetailQuery(UUID.fromString(cartId)), ResponseTypes.instanceOf(CartDetail::class.java)).join()
 }
